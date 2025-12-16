4a187818563ee360e2279ffa8051dbeb64542325
lluambo <luamboojiambo@gmail.com>
2025-12-16 23:36:04 +0100

    thrid commit

--- src/examples/03-flight-booker/index.svelte
+++ src/examples/03-flight-booker/index.svelte
@@ -1,47 +1,52 @@
 <script lang="ts">
-	type Options = 'one-way' | 'return'
+	import { get } from 'svelte/store'
+
+	type Options = 'one-way' | 'round-trip'
 
 	function getDate() {
-		const date = new Date()
-		const [month, day, year] = date
+		const today = new Date()
+		const d = today
 			.toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' })
-			.split('/')
-		return `${year}-${month}-${day}`
+			.split('/') // Format: YYYY-MM-DD
+		return `${d[2]}-${d[0].padStart(2, '0')}-${d[1].padStart(2, '0')}`
 	}
 
 	function handleSubmit(e: Event) {
 		e.preventDefault()
-		alert(`You have booked a ${selected} flight on ${startDate}`)
+		alert(
+			`You have booked a ${selectOptions} flight${selectOptions === 'round-trip' ? ` returning on ${returnDate}` : ''} departing on ${startDate}`,
+		)
 	}
 
-	let selected = $state<Options>('one-way')
+	let selectOptions = $state<Options>('one-way')
 	let startDate = $state(getDate())
 	let returnDate = $state(getDate())
+
+	$inspect({ selectOptions, startDate, returnDate })
 </script>
 
 <form onsubmit={handleSubmit} class="grid-gap">
-	<select bind:value={selected}>
-		<option value="one-way">one-way flight</option>
-		<option value="return">return flight</option>
+	<select bind:value={selectOptions}>
+		<option value="one-way">One-way</option>
+		<option value="round-trip">Round-trip</option>
 	</select>
 
 	<label>
-		<span class="sr-only">Select the start date:</span>
-		<input type="date" bind:value={startDate} min={getDate()} required />
+		<span class="sr-only">Select the start date</span>
+		<input type="date" bind:value={startDate} required />
 	</label>
-
 	<label>
-		<span class="sr-only">Select the return date:</span>
+		<span class="sr-only">Select the return date</span>
 		<input
 			type="date"
 			bind:value={returnDate}
-			min={getDate()}
-			disabled={selected !== 'return'}
-			required
+			required={selectOptions === 'round-trip'}
+			disabled={selectOptions === 'one-way'}
 		/>
 	</label>
-
-	<button type="submit" disabled={!startDate || (selected === 'return' && returnDate < startDate)}>
-		Book
-	</button>
+	<button
+		type="submit"
+		disabled={!startDate || (selectOptions === 'round-trip' && returnDate < startDate)}
+		>Book Flight</button
+	>
 </form>
