889d3066a75908d22a246fdd2b7831dcc8e8d4f1
lluambo <luamboojiambo@gmail.com>
2025-12-23 03:56:23 +0100

    sixth commit

--- src/examples/05-crud/index.svelte
+++ src/examples/05-crud/index.svelte
@@ -5,9 +5,9 @@
 	}
 
 	let people = $state([
-		{ fname: 'Benin', surname: 'Baglehood' },
-		{ fname: 'Calvin', surname: 'Clamble' },
-		{ fname: 'Denise', surname: 'Dingleman' },
+		{ fName: 'Benin', lName: 'Baglehood' },
+		{ fName: 'Calvin', lName: 'Clamble' },
+		{ fName: 'Denise', lName: 'Dingleman' },
 	])
 	let selected = $state<Person>()
 	let person = $state<Person>({ fName: '', lName: '' })
@@ -26,7 +26,7 @@
 		<span class="sr-only">Names:</span>
 		<select bind:value={selected} size="5">
 			{#each people as p}
-				<option value={p}>{p.fname}, {p.surname}</option>
+				<option value={p}>{p.fName}, {p.lName}</option>
 			{/each}
 		</select>
 	</label>
@@ -46,7 +46,7 @@
 		<button
 			onclick={() => {
 				if (person.fName && person.lName) {
-					people = [...people, { fname: person.fName, surname: person.lName }]
+					people = [...people, { fName: person.fName, lName: person.lName }]
 					person = { fName: '', lName: '' }
 				}
 			}}
@@ -57,7 +57,7 @@
 			onclick={() => {
 				if (selected) {
 					people = people.map((p) =>
-						p === selected ? { fname: person.fName, surname: person.lName } : p,
+						p === selected ? { fName: person.fName, lName: person.lName } : p,
 					)
 					selected = undefined
 					person = { fName: '', lName: '' }
@@ -94,5 +94,28 @@
 		gap: 1rem;
 		padding: 1rem;
 		border-radius: 0.5rem;
+
+		.search {
+			grid-area: search;
+		}
+		.group {
+			display: flex;
+			flex-direction: column;
+			gap: 0.25rem;
+		}
+		.people {
+			grid-area: people;
+			width: 100%;
+		}
+		.details {
+			grid-area: details;
+			display: flex;
+			flex-direction: column;
+			gap: 0.5rem;
+		}
+		.actions {
+			grid-area: actions;
+			justify-content: flex-end;
+		}
 	}
 </style>
